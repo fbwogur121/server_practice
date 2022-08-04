de41b690e40096b0b4574e093da98654d6da6e82
fbwogur121 <fbwogur121@naver.com>
2022-08-04 15:54:04 +0900

    commit

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -36,29 +36,3 @@ module.exports = function(app){
 
 
 
-
-
-
-
-
-
-
-// module.exports = function (app) {
-//     const user = require("./userController");
-//     const iwt = require("../../../config/jwtMiddleware");
-
-//     // 0. 테스트 API
-//     app.get('/app/test', user.getTest)
-
-//     // 1. Sign up
-//     app.post("/app/users", user.postUsers);
-
-//     // 2. Get a user info
-//     app.get("/app/users/:userId", iwt, user.getUserById);
-
-//     // 3. Update a user info
-//     app.patch("/app/users/:userId", iwt, user.patchUsers);
-
-//     // 4. Sign in
-//     app.post("/app/login", user.login);
-// };
