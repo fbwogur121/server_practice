1b230b2c20e3821f51bde4ef8ada879b1a6a9e92
fbwogur121 <fbwogur121@naver.com>
2022-08-04 16:02:47 +0900

    commit

--- src/app/User/userProvider.js
+++ src/app/User/userProvider.js
@@ -5,8 +5,8 @@ const userDao = require("./userDao");
 
 // Provider: Read 비즈니스 로직 처리
 
-exports.retrieveUserList = async function (userEmail) {
-  if (!userEmail) {
+exports.retrieveUserList = async function (email) {
+  if (!email) {
     const connection = await pool.getConnection(async (conn) => conn);
     const userListResult = await userDao.selectUser(connection);
     connection.release();
@@ -15,7 +15,7 @@ exports.retrieveUserList = async function (userEmail) {
 
   } else {
     const connection = await pool.getConnection(async (conn) => conn);
-    const userListResult = await userDao.selectUserEmail(connection, userEmail);
+    const userListResult = await userDao.selectUserEmail(connection, email);
     connection.release();
 
     return userListResult;
@@ -31,9 +31,9 @@ exports.retrieveUser = async function (userId) {
   return userResult[0];
 };
 
-exports.emailCheck = async function (userEmail) {
+exports.emailCheck = async function (email) {
   const connection = await pool.getConnection(async (conn) => conn);
-  const emailCheckResult = await userDao.selectUserEmail(connection, userEmail);
+  const emailCheckResult = await userDao.selectUserEmail(connection, email);
   connection.release();
 
   return emailCheckResult;
@@ -49,12 +49,78 @@ exports.passwordCheck = async function (selectUserPasswordParams) {
   return passwordCheckResult[0];
 };
 
-exports.accountCheck = async function (userEmail) {
+exports.accountCheck = async function (email) {
   const connection = await pool.getConnection(async (conn) => conn);
-  const userAccountResult = await userDao.selectUserAccount(connection, userEmail);
+  const userAccountResult = await userDao.selectUserAccount(connection, email);
   connection.release();
 
   return userAccountResult;
 };
 
 
+
+// const { pool } = require("../../../config/database");
+// const { logger } = require("../../../config/winston");
+// const userDao = require("./userDao");
+
+// // check redundant user ID
+// exports.idActiveCheck = async function (id) {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     const userActive = await userDao.selectActiveId(connection, id);
+//     connection.release();
+
+//     return userActive[0];
+// };
+
+// // check redundant of user nickname
+// exports.nicknameActiveCheck = async function (nickname) {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     const userActive = await userDao.selectActiveNickname(connection, nickname);
+//     connection.release();
+
+//     return userActive[0];
+// };
+
+// // check user status
+// exports.accountCheck = async function (id) {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     const userAccountResult = await userDao.selectUserAccount(connection, id);
+//     connection.release();
+
+//     return userAccountResult;
+// };
+
+// // get address range
+// exports.countAddress1 = async function () {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     const addressCount = await userDao.selectCountFromAddress1(connection);
+//     connection.release();
+
+//     return addressCount[0];
+// };
+
+// // retrieve a user
+// exports.retrieveUser = async function (userId) {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     const userResult = await userDao.selectUserFromId(connection, userId);
+//     connection.release();
+
+//     return userResult[0];
+// };
+
+// // check
+// exports.passwordCheck = async function (userId) {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     const passwordCheckResult = await userDao.selectUserPassword(connection, userId);
+//     connection.release();
+
+//     return passwordCheckResult[0];
+// };
+
+// exports.getUserLocation = async function (userIdx, addressType) {
+//     const connection = await pool.getConnection(async (conn) => conn);
+//     const locationResult = await userDao.selectUserLocation(connection, userIdx);
+//     connection.release();
+
+//     return locationResult[0][0][addressType];
+// };
--- src/app/User/userService.js
+++ src/app/User/userService.js
@@ -13,10 +13,10 @@ const {connect} = require("http2");
 
 // Service: Create, Update, Delete 비즈니스 로직 처리
 
-exports.createUser = async function (userEmail, password, nickname) {
+exports.createUser = async function (email, password, nickname) {
     try {
         // 이메일 중복 확인
-        const emailRows = await userProvider.emailCheck(userEmail);
+        const emailRows = await userProvider.emailCheck(email);
         if (emailRows.length > 0)
             return errResponse(baseResponse.SIGNUP_REDUNDANT_EMAIL);
 
@@ -26,7 +26,7 @@ exports.createUser = async function (userEmail, password, nickname) {
             .update(password)
             .digest("hex");
 
-        const insertUserInfoParams = [userEmail, hashedPassword, nickname];
+        const insertUserInfoParams = [email, hashedPassword, nickname];
 
         const connection = await pool.getConnection(async (conn) => conn);
 
@@ -44,13 +44,13 @@ exports.createUser = async function (userEmail, password, nickname) {
 
 
 // TODO: After 로그인 인증 방법 (JWT)
-exports.postSignIn = async function (userEmail, password) {
+exports.postSignIn = async function (email, password) {
     try {
         // 이메일 여부 확인
-        const emailRows = await userProvider.emailCheck(userEmail);
+        const emailRows = await userProvider.emailCheck(email);
         if (emailRows.length < 1) return errResponse(baseResponse.SIGNIN_EMAIL_WRONG);
 
-        const selectEmail = emailRows[0].userEmail
+        const selectEmail = emailRows[0].email
 
         // 비밀번호 확인
         const hashedPassword = await crypto
@@ -66,7 +66,7 @@ exports.postSignIn = async function (userEmail, password) {
         }
 
         // 계정 상태 확인
-        const userInfoRows = await userProvider.accountCheck(userEmail);
+        const userInfoRows = await userProvider.accountCheck(email);
 
         if (userInfoRows[0].status === "INACTIVE") {
             return errResponse(baseResponse.SIGNIN_INACTIVE_ACCOUNT);
@@ -112,3 +112,201 @@ exports.editUser = async function (id, nickname) {
 }
 
 
+
+const { logger } = require("../../../config/winston");
+const { pool } = require("../../../config/database");
+const secret_config = require("../../../config/secret");
+const userProvider = require("./userProvider");
+const userDao = require("./userDao");
+const baseResponse = require("../../../config/baseResponseStatus");
+const { response } = require("../../../config/response");
+const { errResponse } = require("../../../config/response");
+
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const { connect } = require("http2");
+
+// Sign up
+exports.createUser = async function (id, password, name, nickname, addressIdx) {
+    try {
+        // check redundatn ID
+        const isIdActive = await userProvider.idActiveCheck(id);
+        if (isIdActive.active) return errResponse(baseResponse.REDUNDANT_ID);
+
+        // check redundatn PW
+        const isNicknameActive = await userProvider.nicknameActiveCheck(nickname);
+        if (isNicknameActive.active) return errResponse(baseResponse.REDUNDANT_NICKNAME);
+
+        // hash PW
+        const hashedPassword = await crypto.createHash("sha512").update(password).digest("hex");
+
+        // check address range
+        const addressCount = await userProvider.countAddress1();
+        if (addressIdx > addressCount.count || addressIdx < 0) return errResponse(baseResponse.OUT_OF_RANGE_ADDRESSIDX);
+
+        // save hashedPW
+        const insertUserInfoParams = [id, hashedPassword, name, nickname, addressIdx];
+
+        const connection = await pool.getConnection(async (conn) => conn);
+
+        // save
+        const userIdResult = await userDao.insertUserInfo(connection, insertUserInfoParams);
+        console.log(`Added User : ${userIdResult[0].insertId}`);
+
+        connection.release();
+        return response(baseResponse.SUCCESS);
+    } catch (err) {
+        logger.error(`App - createUser Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+
+// login
+exports.postSignIn = async function (id, password) {
+    try {
+        // 계정 상태 확인
+        const userInfoRows = await userProvider.accountCheck(id);
+        if (userInfoRows.length < 1) {
+            return errResponse(baseResponse.NOT_EXIST_ID);
+        } else if (userInfoRows[0].status === "i") {
+            return errResponse(baseResponse.USER_STATUS_INACTIVE);
+        } else if (userInfoRows[0].status === "d") {
+            return errResponse(baseResponse.USER_STATUS_WITHDRAWAL);
+        }
+        // hash PW
+        const hashedPassword = await crypto.createHash("sha512").update(password).digest("hex");
+
+        // check PW
+        const passwordRows = await userProvider.passwordCheck(userInfoRows[0].userId);
+
+        if (passwordRows[0].userPw !== hashedPassword) {
+            return errResponse(baseResponse.NOT_MATCHED_PASSWORD);
+        }
+
+        // create token
+        let token = await jwt.sign(
+            {
+                userId: userInfoRows[0].userId,
+                userIdx: userInfoRows[0].userIdx,
+            }, // payload
+            secret_config.jwtsecret, // secret key
+            {
+                expiresIn: "365d",
+                subject: "userInfo",
+            }
+        );
+
+        return response(baseResponse.SUCCESS, { userId: userInfoRows[0].userId, jwt: token });
+    } catch (err) {
+        logger.error(`App - postSignIn Service error\n: ${err.message} \n${JSON.stringify(err)}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+
+exports.patchNickname = async function (userId, nickname) {
+    try {
+        // nickname length
+        if (nickname.length > 24) return res.send(errResponse(baseResponse.LENGTH_NICKNAME));
+
+        // nickname redundant
+        const isNicknameActive = await userProvider.nicknameActiveCheck(nickname);
+        if (isNicknameActive.active) return errResponse(baseResponse.REDUNDANT_NICKNAME);
+
+        // Dao
+        const connection = await pool.getConnection(async (conn) => conn);
+        const updateNicknameParams = [nickname, userId];
+        const updateNicknameResult = await userDao.updateUserNickname(connection, updateNicknameParams);
+        connection.release();
+
+        return response(baseResponse.SUCCESS);
+    } catch (err) {
+        logger.error(`App - patchNickname Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+
+exports.patchPassword = async function (userId, password) {
+    try {
+        //password validation
+        if (password.length > 20 || password.length < 6) return errResponse(baseResponse.LENGTH_PASSWORD);
+
+        // hash PW
+        const hashedPassword = await crypto.createHash("sha512").update(password).digest("hex");
+
+        const userPasswordRows = await userProvider.passwordCheck(userId);
+        if (userPasswordRows[0].userPw === hashedPassword) {
+            return errResponse(baseResponse.SAME_PASSWORD);
+        }
+
+        // Dao
+        const connection = await pool.getConnection(async (conn) => conn);
+        const updatePasswordParams = [hashedPassword, userId];
+        const updatePasswordResult = await userDao.updateUserPassword(connection, updatePasswordParams);
+        connection.release();
+
+        return response(baseResponse.SUCCESS);
+    } catch (err) {
+        logger.error(`App - patchPassword Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+
+exports.patchAddressIdx = async function (userId, addressIdx) {
+    try {
+        // address validation
+        const maxAddIdx = await userProvider.countAddress1();
+        if (addressIdx > maxAddIdx.count || addressIdx < 1) return errResponse(baseResponse.OUT_OF_RANGE_ADDRESSIDX);
+
+        // Dao
+        const connection = await pool.getConnection(async (conn) => conn);
+        const updateAddressParams = [addressIdx, userId];
+        const updateAddressResult = await userDao.updateUserAddress(connection, updateAddressParams);
+        connection.release();
+
+        return response(baseResponse.SUCCESS);
+    } catch (err) {
+        logger.error(`App - patchAddressIdx Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+exports.patchSubAddressIdx = async function (userId, subAddressIdx) {
+    try {
+        // address validation
+        const maxAddIdx = await userProvider.countAddress1();
+        if (subAddressIdx > maxAddIdx.count || subAddressIdx < 1) return errResponse(baseResponse.OUT_OF_RANGE_ADDRESSIDX);
+
+        // Dao
+        const connection = await pool.getConnection(async (conn) => conn);
+        const updateSubAddressParams = [subAddressIdx, userId];
+        const updateSubAddressResult = await userDao.updateUserAddress(connection, updateSubAddressParams);
+        connection.release();
+
+        return response(baseResponse.SUCCESS);
+    } catch (err) {
+        logger.error(`App - patchSubAddressIdx Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
+
+exports.patchStatus = async function (userId, status) {
+    try {
+        // status validation
+        const statusArray = ["a", "i", "d"];
+        const statusValidation = statusArray.some((element) => status === element);
+        if (!statusValidation) return errResponse(baseResponse.OUT_OF_RANGE_USER_STATUS);
+
+        const userInfoRows = await userProvider.accountCheck(userId);
+        if (userInfoRows[0].status === status) return errResponse(baseResponse.SAME_STATUS);
+
+        // Dao
+        const connection = await pool.getConnection(async (conn) => conn);
+        const updateStatusParams = [status, userId];
+        const updateStatusResult = await userDao.updateUserStatus(connection, updateStatusParams);
+        connection.release();
+
+        return response(baseResponse.SUCCESS);
+    } catch (err) {
+        logger.error(`App - patchStatus Service error\n: ${err.message}`);
+        return errResponse(baseResponse.DB_ERROR);
+    }
+};
