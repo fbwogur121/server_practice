51f1a6cdbd614af2fd54b6406a403b5f5bfacbd8
fbwogur121 <fbwogur121@naver.com>
2022-08-05 19:54:08 +0900

    commit

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -120,9 +120,9 @@ async function selectUserAccount(connection, id) {
 // 이메일로 회원 조회
 async function selectUserEmail(connection, email) {
   const selectUserEmailQuery = `
-                SELECT email, nickname 
-                FROM UserInfo 
-                WHERE email = ?;
+                SELECT userEmail, userNickName 
+                FROM User
+                WHERE userEmail = ?;
                 `;
   const [emailRows] = await connection.query(selectUserEmailQuery, email);
   return emailRows;
